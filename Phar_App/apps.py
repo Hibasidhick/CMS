447644181892c22f4b49fc0d40333c3ba2f582d9
Hiba <sidhickhibasidick@gmail.com>
2025-04-02 17:36:03 +0530

    Files Added Successfully

---
+++ Phar_App/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PharAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Phar_App'
---
+++ Recep_App/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RecepAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Recep_App'
